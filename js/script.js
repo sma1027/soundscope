60687f5b570cf0e85e4e4b19327df955dcae2df3
sofia ma <sofia.ma@gmail.com>
2015-01-06 12:47:59 -0500

    added weeks and edited release date

--- js/script.js
+++ js/script.js
@@ -13,8 +13,14 @@ $(document).ready(function() {
   new WOW().init();
     
   /* countdown */
-  $('.timer').countdown("2015/01/31", function(event){
-    $('.days').html(event.strftime('%D'));
+  $('.timer').countdown("02/21/2015 12:00:00", function(event){
+    // debugger;
+    $('.release-date').html(event.finalDate.toDateString());
+    // $('.month').html(event.finalDate.getMonth()+1);
+    // $('.date').html(event.finalDate.getDate());
+    // $('.year').html(event.finalDate.getFullYear());
+    $('.weeks').html(event.strftime('%w'));
+    $('.days').html(event.strftime('%d'));
     $('.hours').html(event.strftime('%H'))
     $('.minutes').html(event.strftime('%M'));
     $('.seconds').html(event.strftime('%S'));
