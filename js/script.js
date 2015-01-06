3dbb42ab86fddd48542418b1218380acbd05934d
sofia ma <sofia.ma@gmail.com>
2015-01-06 02:07:21 -0500

    added countdown

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
-jQuery(document).ready(function() {
+$(document).ready(function() {
   
-  /* Fullscreen background */
-  $.backstretch([
+  /* fullscreen background */
+  $('body').backstretch([
       "img/backgrounds/1.jpg",
       "img/backgrounds/2.jpg",
       "img/backgrounds/16.jpg"
     ], {duration: 3000, fade: 750});
 
-  /* Wow */
+  /* wow */
   new WOW().init();
     
+  /* countdown */
+  $('.timer').countdown("2015/01/31", function(event){
+    $(this).text(
+      event.strftime('%D %H hours %M minutes %S seconds')
+    );
+  });
+
 });
